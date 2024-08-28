da281e4cb7b074ed5c230c249e206ac40c518d4e
CalebKiK <karimicaleb@gmail.com>
2024-08-28 13:20:14 +0300

    Complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_nums = [n for n in num_list if n % 2 == 0]
+    if len(even_nums) < 1:
+        print([])
+        return []
+    
+    else:
+        print(even_nums)
+        return even_nums
 
 def make_exclamation(sentence_list):
-    pass
+    if len(sentence_list) <= 0:
+        print([])
+        return []
+    
+    else:
+        string_exclamation = [word + "!" for word in sentence_list]
+        print(string_exclamation)
+        return string_exclamation
